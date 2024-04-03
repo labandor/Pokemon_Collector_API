01a2fb69d60f1ecac7c755e82765ac88434f9a42
Laband <lukeabandeen@gmail.com>
2024-04-03 09:45:03 -0400

    added toy crud

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, PokemonList, PokemonDetail, FeedingListCreate, FeedingDetail
+from .views import Home, PokemonList, PokemonDetail, FeedingListCreate, FeedingDetail, ToyListCreate, ToyDetail, ToyList
 
 urlpatterns = [
         path('', Home.as_view(), name='home'),
@@ -7,5 +7,8 @@ urlpatterns = [
         path('pokemon/<int:id>/', PokemonDetail.as_view(), name='pokemon-detail'),
         path('pokemon/<int:pokemon_id>/feedings/', FeedingListCreate.as_view(), name='feeding-list-create'),
         path('pokemon/<int:pokemon_id>/feedings/<int:id>/', FeedingDetail.as_view(), name='feeding-detail'),
+        path('pokemon/<int:pokemon_id>/toys', ToyListCreate.as_view(), name='toy-list-create'),
+        path('pokemon/toys/<int:id>/', ToyDetail.as_view(), name='toy-detail'),
+        path('pokemon/toys', ToyList.as_view(), name='toy-list'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Pokemon, Feeding
-from .serializers import PokemonSerializer, FeedingSerializer
+from .models import Pokemon, Feeding, Toy
+from .serializers import PokemonSerializer, FeedingSerializer, ToySerializer
 from rest_framework import generics, filters
 
 class Home(APIView):
@@ -40,4 +40,26 @@ class FeedingDetail(generics.RetrieveUpdateDestroyAPIView):
   def get_queryset(self):
     pokemon_id = self.kwargs['pokemon_id']
     return Feeding.objects.filter(pokemon_id=pokemon_id)
+
+class ToyList(generics.ListCreateAPIView):
+    queryset = Toy.objects.all()
+    serializer_class = ToySerializer
+
+class ToyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Toy.objects.all()
+    serializer_class = ToySerializer
+    lookup_field = 'id'
+
+class ToyListCreate(generics.ListCreateAPIView):
+    serializer_class = ToySerializer
+
+    def get_queryset(self):
+        pokemon_id = self.kwargs['pokemon_id']
+        return Toy.objects.filter(pokemon_id = pokemon_id)
+
+#    def perform_create(self, serializer):
+#        pokemon_id = self.kwargs['pokemon_id']
+#        pokemon = Pokemon.objects.get(id=pokemon_id)
+#        serializer.save(pokemon=pokemon)
+
 
